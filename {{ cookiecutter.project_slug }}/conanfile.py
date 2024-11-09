0de4b46d9514d18ec5272e5d501680fd96c95e71
GaiBrutman <gai.brutman@gmail.com>
2024-11-09 18:04:06 +0200

    Format conanfile.py

--- {{ cookiecutter.project_slug }}/conanfile.py
+++ {{ cookiecutter.project_slug }}/conanfile.py
@@ -1,13 +1,15 @@
 from pathlib import Path
 import re
 from conan import ConanFile
-from conan.tools.meson import Meson, MesonToolchain
+from conan.tools.meson import Meson
 from conan.tools.layout import basic_layout
 
-def get_project_option_from_meson(option: str):
+
+def get_project_option_from_meson(option: str) -> str:
     return re.search(
-        fr"\s*{option}\s*:\s*\'(.*)\'", Path("meson.build").read_text()
+        rf"\s*{option}\s*:\s*\'(.*)\'", Path("meson.build").read_text()
     ).group(1)
+
 
 class PkgConan(ConanFile):
     name = "{{ cookiecutter.project_slug }}"
