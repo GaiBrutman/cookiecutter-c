875747887b68d8ac9869fe296c02bf56dbd69500
GaiBrutman <gai.brutman@gmail.com>
2024-11-09 16:16:37 +0200

    Add project type handling in post-generation script and create main.c for executable projects

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,3 +4,8 @@ import pathlib
 if __name__ == '__main__':
     if 'Not open source' == '{{ cookiecutter.license }}':
         pathlib.Path('LICENSE').unlink()
+
+    if 'Library' != '{{ cookiecutter.project_type }}':
+        pathlib.Path('src/{{ cookiecutter.project_slug }}.c').unlink()
+    elif 'Executable' != '{{ cookiecutter.project_type }}':
+        pathlib.Path('src/main.c').unlink()
---
+++ {{ cookiecutter.project_slug }}/src/main.c
@@ -0,0 +1,4 @@
+int main(void) {
+    // Add your code here
+    return 0;
+}
