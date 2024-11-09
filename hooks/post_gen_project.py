10def9b35aeeef3863a3c3732e5e1b7c0e1186d7
GaiBrutman <gai.brutman@gmail.com>
2024-11-09 18:32:25 +0200

    Add header file for library projects

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -7,5 +7,8 @@ if __name__ == "__main__":
 
     if "Library" != "{{ cookiecutter.project_type }}":
         pathlib.Path("src/{{ cookiecutter.project_slug }}.c").unlink()
+        pathlib.Path(
+            "include/{{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}.h"
+        ).unlink()
     elif "Executable" != "{{ cookiecutter.project_type }}":
         pathlib.Path("src/main.c").unlink()
